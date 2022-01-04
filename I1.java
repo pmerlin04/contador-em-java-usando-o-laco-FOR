33e259195fffde62ba64aa86ed8da4b03ecff365
pmerlin04 <92960530+pmerlin04@users.noreply.github.com>
2022-01-04 14:57:52 -0300

    Add files via upload

---
+++ I1.java
@@ -0,0 +1,24 @@
+package exerciciosComandoFor;
+
+public class I1 {
+
+	public static void main(String[] args) {
+		int numero;
+		
+
+				for (numero = 1; numero<=100; ++numero) {
+					System.out.println("numero - " + numero);
+				
+				}
+	
+					for (numero = 100; numero >=1; numero--) {
+						System.out.println("numero - " + numero);
+					}
+			
+				//ESTE E O LACO DE REPETICAO  "PARA" IGUAL O DO VISUALG
+				//ESTE ++c SIGNFICA QUE A VARIAVEL I VAI ACOMPANHAR O NUMERO, EX: 1, 2, 3, 4...
+				//A CONDICAO c<101 SIGNIFICA QUE O NUMERO SO VAI ATE O 100
+				//C = CRESCENTE
+		
+	}
+}
